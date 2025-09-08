076a63ae9015c7acac15ec3ff23f79323bc81b42
eliane <eliane@evgregory.de>
2025-09-08 11:27:36 +0200

    change methods to private that don't need to be public

--- src/app/bnb/bnb_job.hpp
+++ src/app/bnb/bnb_job.hpp
@@ -15,6 +15,7 @@ class BnbJob : public Job {
 
 private:
     JobResult _result;
+
     size_t _nr_tasks;
     int _nr_processors;
     
@@ -25,31 +26,40 @@ private:
     };
 
     std::queue<Work> _work_queue;
+
+    AdjustablePermutation _perm; // Represents a pseudo-random permutation of a set of integers [0..n).
+
     Work _best_solution;
     int _best_length;
+
     Mutex solution_mtx;
     Mutex queue_mtx;
     ConditionVariable _loop_cond_var;
+
     bool _working;
     bool _waiting;
     bool _finished;
+    bool _send_messages {false}; // Whether we can send messages
 
     static const int MSG_ROUNDTRIP = 1; // internal message tag for our round-trip messages
     static const int MSG_TEST = 2;
     static const int MSG_WORK_STEALING_QUERY = 31;
     static const int MSG_WORK_STEALING_ANSWER = 32;
 
-    static const int NUM_WORKERS = 3; // # workers we request and require
+    static const int NUM_WORKERS = 2; // # workers we request and require   
+    
+    void init();
+    void loop();
 
-    // Represents a pseudo-random permutation of a set of integers [0..n).
-    AdjustablePermutation _perm;
+    Work branch(Work& work);
 
-    // Whether we can send messages
-    bool _send_messages {false};
-
-    void insertResult(int resultCode, const std::vector<int>& solution);
     std::vector<int> splitQueue();
     void addToQueue(std::vector<int>& message);
+
+    std::vector<int> compute_processor_length(const std::vector<std::vector<int>>& processors);
+    std::string transform_for_log(const std::string& reason, const Work& work);
+
+    void insertResult(int resultCode, const std::vector<int>& solution);
 
 public:
     BnbJob(const Parameters& params, const JobSetup& setup, AppMessageTable& table);
@@ -65,13 +75,7 @@ public:
     bool appl_isDestructible() override {return true;}
     void appl_memoryPanic() override {}
 
-    int getDemand() const override;
-
-    void init();
-    void loop();
-    Work branch(Work& work);
-    std::vector<int> compute_processor_length(const std::vector<std::vector<int>>& processors);
-    std::string transform_for_log(const std::string& reason, const Work& work);
+    int getDemand() const override;    
 };
 
 #endif
