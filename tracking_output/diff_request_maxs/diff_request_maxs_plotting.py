f26cb019985a0c2e224ad82278290b4d752b67d2
eliane <eliane@evgregory.de>
2026-01-26 16:06:54 +0100

    diff_request_max plot

--- tracking_output/diff_request_maxs/diff_request_maxs_plotting.py
+++ tracking_output/diff_request_maxs/diff_request_maxs_plotting.py
@@ -1,5 +1,87 @@
 from matplotlib import pyplot as plt
 import math
+from collections import defaultdict
+import os
+
+# Assign directory
+dir_list = [r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/scripts/bnb/out/", 
+            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/diff_request_maxs/max_2",
+            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/diff_request_maxs/max_20"]
+
+# Variables
+non_tracking_files = []
+values = defaultdict(list)
+
+# Iterate over files in directory
+for directory in dir_list:
+    for name in os.listdir(directory):
+        tracking_lines = []
+        tracking_values = []
+        finished_values = []
+        wanted_keyword = "Percentage"
+        file_path = os.path.join(directory, name)
+
+        if not os.path.isfile(file_path):
+            continue
+        
+        with open(file_path) as file:
+            # Delete non-solved files
+            if not "[solved]" in file.read():
+                non_tracking_files.append(name)
+                continue
+
+            file.seek(0)
+
+            # Add all lines relevant for tracking
+            for line in file:
+                line.strip()
+                if "[tracking]"  in line:
+                    tracking_lines.append(line)
+        
+        # Delete line delimiters and [tracking] keyword
+        for line in tracking_lines:
+            if wanted_keyword in line:
+                line = line[:-1]
+                line = line.split(" ")
+                line = line[1:3] + line[4:]
+                tracking_values.append(line)
+                print(line)
+
+        # Probably later prettier but in basic the only necessary numbers are the timestamp line[0] and percentage line[-1]
+        for line in tracking_values:
+            finished_values.append(float(line[-1]))
+        
+        # Decide which values are important for the current plot and add them to the dict
+        label = name.split("_")[1]
+        for line in finished_values:
+            values[label].append(line)
+    
+
+# Print non solved files
+if not non_tracking_files:
+    print("All files were solved")
+for name in non_tracking_files:
+    print(name+" was not solved")
+
+print(values)
+myList = sorted(values.items())
+x, y = zip(*myList)
+values = list(map(int, x)), y
+print(values)
+
+max_value = math.ceil(max(max(sub_list) for sub_list in values[1]))
+min_value = math.floor(min(min(sub_list) for sub_list in values[1]))
+
+plt.boxplot(x=values[1], tick_labels=[2, 20, 200])
+
+plt.xlim([0, len(values[0])+1])
+plt.ylim([min_value, 0.1])
+
+plt.title("Compare if changing the maximum \n number of jobs a worker can steal does anything")
+plt.ylabel("Utilization")
+plt.xlabel("Maximum")
+
+plt.savefig("tracking_output/diff_request_maxs/plot_perc_all_2.png")
 
 def get_values(textfile):
     tracking_lines = []
@@ -101,5 +183,3 @@ def explored_nodes():
     plt.xlabel("Maximum")
 
     plt.savefig("diff_request_maxs/plot_nodes.png")
-
-explored_nodes()
