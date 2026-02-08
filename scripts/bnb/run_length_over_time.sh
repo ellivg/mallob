7ddddedf361c1b7b980a1ad95c73ec97cf9a188d
eliane <eliane@evgregory.de>
2026-02-08 20:13:15 +0100

    fix script

--- scripts/bnb/run_length_over_time.sh
+++ scripts/bnb/run_length_over_time.sh
@@ -69,7 +69,7 @@ for ((i=1; i<=INSTANCES; i++)); do
   echo "" 
   echo ""
 
-  for ((j=1; j<=5; j++)); do
+  for ((j=1; j<=10; j++)); do
     echo "Run Nr $j"
     mpirun -np $MPI_PROCESSES --bind-to core --map-by ppr:${MPI_PROCESSES}:node:pe=${THREADS_PER_PROCESS} build/mallob $MY_MALLOB_OPTIONS >scripts/bnb/out/"file_"$i"_"$j".txt"
   done
