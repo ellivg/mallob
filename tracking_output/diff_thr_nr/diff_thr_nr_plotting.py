c05b0719862ca0609e195dde597ec5b950b04347
eliane <eliane@evgregory.de>
2026-02-12 15:04:56 +0100

    new diff_thr_nr plotting

--- tracking_output/diff_thr_nr/diff_thr_nr_plotting.py
+++ tracking_output/diff_thr_nr/diff_thr_nr_plotting.py
@@ -7,84 +7,137 @@ import os
 dir_list = [r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/diff_thr_nr/out",
             r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/diff_thr_nr/out4"]
 
-# Variables
-non_tracking_files = []
-values = defaultdict(list)
+def get_values(wanted_keyword=str, index=int, median=bool, ylabel=str, fig=str):
+    # Variables
+    non_tracking_files = []
+    values = defaultdict(list)
 
-# Iterate over files in directory
-for directory in dir_list:
-    for name in os.listdir(directory):
-        tracking_lines = []
-        tracking_values = []
-        finished_values = []
-        wanted_keyword = "solved"
-        file_path = os.path.join(directory, name)
+    # Iterate over files in directory
+    for directory in dir_list:
+        for name in os.listdir(directory):
+            tracking_lines = []
+            tracking_values = []
+            finished_values = []
+            file_path = os.path.join(directory, name)
 
-        if not os.path.isfile(file_path):
-            continue
-        
-        with open(file_path) as file:
-            #print("Opening: "+name)
-            # Delete non-solved files
-            if not "[solved]" in file.read():
-                non_tracking_files.append(name)
+            if not os.path.isfile(file_path):
                 continue
+            
+            with open(file_path) as file:
+                #print("Opening: "+name)
+                # Delete non-solved files
+                if not "[solved]" in file.read():
+                    non_tracking_files.append(name)
+                    continue
 
-            file.seek(0)
+                file.seek(0)
 
-            # Add all lines relevant for tracking
-            for line in file:
-                line.strip()
-                if wanted_keyword in line:
-                    tracking_lines.append(line)
-        
-        # print(tracking_lines)
-        
-        # Delete line delimiters and [tracking] keyword
-        for line in tracking_lines:
-            line = line[:-1]
-            line = line.split(" ")
-            line = line[1:3] + line[4:]
-            tracking_values.append(line)
-        
-        # print(tracking_values)
+                # Add all lines relevant for tracking
+                for line in file:
+                    line.strip()
+                    if wanted_keyword in line:
+                        tracking_lines.append(line)
+            
+            #print(tracking_lines)
+            
+            # Delete line delimiters and [tracking] keyword
+            for line in tracking_lines:
+                line = line[:-1]
+                line = line.split(" ")
+                line = line[1:3] + line[4:]
+                tracking_values.append(line)
+            
+            #print(tracking_values)
 
-        # Probably later prettier but in basic the only necessary numbers are the timestamp line[0] and percentage line[-1]
-        for line in tracking_values:
-            finished_values.append(float(line[0]))
+            # Only use the relevant line (usually index=0 or index=-1)
+            for line in tracking_values:
+                finished_values.append(float(line[index]))
 
-        #print(finished_values)
-        
-        # Decide which values are important for the current plot and add them to the dict
-        label = name.split("_")[1]
-        for line in finished_values:
-            values[label].append(line)
-        
-        #print(values)
+            #print(finished_values)
 
-# Print non solved files
-if not non_tracking_files:
-    print("All files were solved")
-for name in non_tracking_files:
-    print(name+" was not solved")
+            # Get label
+            label = name.split("_")[1]
+            #print(label)
 
-print(values)
-myList = sorted(values.items())
-x, y = zip(*myList)
-values = list(map(int, x)), y
-print(values)
+            if median:
+                compute = finished_values
+                finished_values = []
 
-max_value = math.ceil(max(max(sub_list) for sub_list in values[1]))
-min_value = math.floor(min(min(sub_list) for sub_list in values[1]))
+                number = sum(compute) / float(label)
+                finished_values.append(number)
 
-plt.boxplot(x=values[1], tick_labels=values[0])
+                #print(number)
+                
+            
+            # Decide which values are important for the current plot and add them to the dict
+            for line in finished_values:
+                values[label].append(line)
+            
+            print(values)
 
-plt.xlim([0, len(values[0])+1])
-plt.ylim([0, 0.25])
+    # Print non solved files
+    if not non_tracking_files:
+        print("All files were solved")
+    for name in non_tracking_files:
+        print(name+" was not solved")
 
-plt.title("Compare if changing the number of threads \n does anything on a small example")
-plt.ylabel("Time")
-plt.xlabel("Number Of Threads")
+    print(values)
+    myList = sorted(values.items())
+    x, y = zip(*myList)
+    values = list(map(int, x)), y
+    print(values)
 
-plt.savefig("tracking_output/diff_thr_nr/diff_thr_nr_yy_zoom.png")
+    max_value = math.ceil(max(max(sub_list) for sub_list in values[1]))
+    min_value = math.floor(min(min(sub_list) for sub_list in values[1]))
 
+    plt.boxplot(x=values[1], tick_labels=values[0])
+
+    plt.xlim([0, len(values[0])+1])
+    plt.ylim([0, 0.05])
+
+    plt.title("Compare if changing the number of threads \n does anything on a small example")
+    plt.ylabel(ylabel)
+    plt.xlabel("Number Of Threads")
+
+    fig_name = "tracking_output/diff_thr_nr/diff_thr_nr_"+fig+"_zoom.png"
+    plt.savefig(fig_name)
+
+def time():
+    print("Run Time")
+
+    wanted_keyword = "solved"
+    index = 0
+    median = False
+    ylabel = "Time"
+    fig = "time"
+
+    get_values(wanted_keyword, index, median, ylabel, fig)
+    
+
+def expl():
+    print("Run Explored Nodes")
+    
+    wanted_keyword = "explored nodes"
+    index = -1
+    median = True
+    ylabel = "Median Explored Nodes"
+    fig = "expl"
+    
+    get_values(wanted_keyword, index, median, ylabel, fig)
+
+def perc():
+    print("Run Percentage")
+
+    wanted_keyword = "time spent working"
+    index = -1
+    median = False
+    ylabel = "Percentage Spent Working"
+    fig = "perc"
+    
+    get_values(wanted_keyword, index, median, ylabel, fig)
+
+
+# Run all
+#time()
+#expl()
+perc()
