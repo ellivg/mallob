b5a77904acf6e9cc30e0f47ce66ff0c1aa3dbcee
eliane <eliane@evgregory.de>
2026-02-28 15:19:25 +0100

    new speedup evaluation

--- tracking_output/speedup/speedup_plotting.py
+++ tracking_output/speedup/speedup_plotting.py
@@ -5,7 +5,7 @@ import math
 import os
 
 # Assign directory
-dir_list = [r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/speedup/in20"]
+dir_list = [r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/speedup/in30"]
 
 # Variables
 non_tracking_files = []
