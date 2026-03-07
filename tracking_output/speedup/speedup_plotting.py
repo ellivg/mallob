1c4d179e06c50c5372d48f73135da0ec2995523a
eliane <eliane@evgregory.de>
2026-03-07 18:44:18 +0100

    new speedup plot

--- tracking_output/speedup/speedup_plotting.py
+++ tracking_output/speedup/speedup_plotting.py
@@ -5,16 +5,26 @@ import math
 import os
 
 # Assign directory
-dir_list = [r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/speedup/in30"]
+dir_list = [r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/speedup/in10",
+            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/speedup/in15",
+            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/speedup/in20",
+            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/speedup/in25_1",
+            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/speedup/in25_2",
+            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/speedup/in30"]
 
 # Variables
 non_tracking_files = []
-values = defaultdict(list)
+all_values = []
 wanted_keyword = "RESPONSE_TIME"
+num_files = 0
 
 # Iterate over files in directory
 for directory in dir_list:
+    values = defaultdict(list)
+    
+
     for name in os.listdir(directory):
+        num_files += 1
         tracking_lines = []
         tracking_values = []
         finished_values = []
@@ -65,27 +75,38 @@ for directory in dir_list:
         
         #print(values)
 
+    #print(values)
+    myList = sorted(values.items())
+    x, y = zip(*myList)
+    values = list(map(int, x)),  list(y)
+    #print(values)
+
+    for i in range(0,2):
+        sum = np.log(values[1][i])
+        gmean = np.exp(sum.mean())
+        values[1][i] = gmean
+    
+    speedup = values[1][0] / values[1][1]
+    all_values.append((values[1][1], speedup))
+
+
 # Print non solved files
 if not non_tracking_files:
     print("All files were solved")
-for name in non_tracking_files:
-    print(name+" was not solved")
+percent = len(non_tracking_files) / num_files
+print(str(percent)+"% was not solved")
 
 #print(values)
-myList = sorted(values.items())
-x, y = zip(*myList)
-values = list(map(int, x)),  list(y)
+x, y = zip(*all_values)
 
-print(values)
+plt.plot([0,300], [1,1], color="black")
+plt.plot(x, y, color="red")
 
-for i in range(0,2):
-    sum = np.log(values[1][i])
-    gmean = np.exp(sum.mean())
-    values[1][i] = gmean
+plt.xlim([0, 150])
+plt.ylim([0, 2])
 
-speedup = values[1][0] / values[1][1]
+plt.ylabel("Speedup")
+plt.xlabel("T_parallel")
 
-print("")
-print("T_seq: "+str(values[1][0]))
-print("T_par: "+str(values[1][1]))
-print("Speedup: "+str(speedup))
+fig_name = "tracking_output/speedup/speedup.png"
+plt.savefig(fig_name)
