27c5c9bf7fce67a9d78c85407fe9b6c9c625172f
eliane <eliane@evgregory.de>
2026-03-13 17:45:37 +0100

    run instances

--- scripts/bnb/run_cdf.sh
+++ scripts/bnb/run_cdf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 THREADS_PER_PROCESS=1 # Set to desired number
-INSTANCES=10 #TODO: Set to desired number (or count paths in paths.txt file)
+INSTANCES=4 #TODO: Set to desired number (or count paths in paths.txt file)
 
 echo "" 
 echo ""
