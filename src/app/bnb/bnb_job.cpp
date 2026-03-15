8ceb16d84f9a64fecc0c31c03a609a2c0706e1bc
eliane <eliane@evgregory.de>
2026-03-15 16:47:48 +0100

    change tracking

--- src/app/bnb/bnb_job.cpp
+++ src/app/bnb/bnb_job.cpp
@@ -400,7 +400,7 @@ void BnbJob::loop() {
                 // Switch tracker
                 tracker.curr_time = Timer::elapsedSeconds();
                 tracker.time_spent_outside += tracker.curr_time - tracker.wait_full_time;
-                tracker.time_spent_get_work = tracker.curr_time;
+                tracker.wait_full_time = tracker.curr_time;
                 LOG(V2_INFO, "[track] End outside & Start wait full\n");
 
                 LOG(V2_INFO, "[track] End wait full & Start get work\n");
