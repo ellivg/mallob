a163de6c1020139f23832776e64639ab6488f7cb
eliane <eliane@evgregory.de>
2026-03-16 07:29:58 +0100

    new plots

--- tracking_output/cdf/cdf_plotting.py
+++ tracking_output/cdf/cdf_plotting.py
@@ -13,7 +13,8 @@ dir_list = [r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_ou
             r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/all_in_run7",
             r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/all_in_run8",
             r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/all_in_run9",
-            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/all_in_run10"]
+            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/all_in_run10",
+            r"/home/eliane/Documents/Bachelorarbeit/MY MALLOB/mallob/tracking_output/all_in_run11"]
 
 # Variables
 non_tracking_files = []
@@ -95,11 +96,11 @@ for i in range(0, len(values[0])):
 
 plt.xscale('log')
 #plt.xlim([0, 300]) # max should be 300secs
-plt.ylim([0, 190]) # nr of examples times 10
+plt.ylim([0, 200]) # nr of examples times 10
 
 plt.ylabel("# of instances solved")
 plt.xlabel("run time [s]")
 plt.legend()
 
-fig_name = "tracking_output/cdf/cdf_plot19_log.pdf"
+fig_name = "tracking_output/cdf/cdf_plot20_log.pdf"
 plt.savefig(fig_name, format="pdf")
